e06aa516a00687fffda633d49ac8b46028b93aed
Cypher Cola <cypherco@student.21-school.ru>
2024-12-07 16:26:34 +0900

    asd

--- demo/src/main/java/com/example/demo/data/Peer.java
+++ demo/src/main/java/com/example/demo/data/Peer.java
@@ -4,5 +4,6 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
 
+
 @Table("peer_data")
 public record Peer(@Id String name, PeerState state, int wave, int intensive, int xp, @Column("peer_review_points") int peerReviewPoints, @Column("code_review_points") int codeReviewPoints, int coins) {}
