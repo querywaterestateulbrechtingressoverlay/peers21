ecbade33427bd899e49fbdf5b27959ec2e1034b4
Cypher Cola <cypherco@student.21-school.ru>
2024-12-07 16:59:35 +0900

    asd

--- demo/src/main/resources/schema.sql
+++ demo/src/main/resources/schema.sql
@@ -1,2 +1,2 @@
-CREATE TABLE peer_data (name varchar(8) PRIMARY KEY, state varchar(7), wave int , intensive int, xp int, peer_review_points int, code_review_points int, coins int );
+CREATE TABLE peer_data ("name" varchar(8) PRIMARY KEY, "state" varchar(7), "wave" int, "intensive" int, "xp" int, "peer_review_points" int, "code_review_points" int, "coins" int);
 --INSERT INTO peer_data VALUES ('cypherco', 'ALIVE', 2, 4, 999999, 999999, 9999999, 9999999);
