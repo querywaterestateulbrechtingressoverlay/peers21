c311604618a81227b230d283bbea733ef720cab8
Cypher Cola <cypherco@student.21-school.ru>
2024-12-09 18:44:56 +0900

    asd

--- demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,11 +1,21 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+
+import static net.bytebuddy.matcher.ElementMatchers.is;
+import static net.bytebuddy.matcher.ElementMatchers.not;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest(
   webEnvironment = SpringBootTest.WebEnvironment.MOCK,
-  classes = Application.class)
+  classes = DemoApplication.class)
 @AutoConfigureMockMvc
 class DemoApplicationTests {
 	@Autowired
@@ -16,12 +26,13 @@ class DemoApplicationTests {
 
 	}
 	@Test
-	void getPeers() {
+	void getPeers() throws Exception {
 	 mvc.perform(get("/api/peers")
       .contentType(MediaType.APPLICATION_JSON))
       .andExpect(status().isOk())
       .andExpect(content()
       .contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
-      .andExpect(jsonPath("$[0].name", is("cypherco")))
-      .andExpect(jsonPath("$[0].xp", !is(999999)));}
+      .andExpect((ResultMatcher) jsonPath("$[0].name", is("cypherco")))
+      .andExpect((ResultMatcher) jsonPath("$[0].xp", not(is("999999"))));
+	}
 }
