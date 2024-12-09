a53a10f58a134cd9cd351428acf7886946024751
Cypher Cola <cypherco@student.21-school.ru>
2024-12-09 17:11:48 +0900

    asd

--- demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -3,11 +3,25 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(
+  webEnvironment = SpringBootTest.WebEnvironment.MOCK,
+  classes = Application.class)
+@AutoConfigureMockMvc
 class DemoApplicationTests {
+	@Autowired
+    private MockMvc mvc;
+
 	@Test
 	void contextLoads() {
 
 	}
-
+	@Test
+	void getPeers() {
+	 mvc.perform(get("/api/peers")
+      .contentType(MediaType.APPLICATION_JSON))
+      .andExpect(status().isOk())
+      .andExpect(content()
+      .contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+      .andExpect(jsonPath("$[0].name", is("cypherco")))
+      .andExpect(jsonPath("$[0].xp", !is(999999)));}
 }
