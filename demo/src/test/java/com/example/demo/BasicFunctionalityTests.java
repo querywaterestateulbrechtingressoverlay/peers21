5688ec934622feb393de1720985e71aa60de2723
Cypher Cola <cypherco@student.21-school.ru>
2025-01-09 08:21:04 +0900

    tests started

--- demo/src/test/java/com/example/demo/BasicFunctionalityTests.java
+++ demo/src/test/java/com/example/demo/BasicFunctionalityTests.java
@@ -10,14 +10,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.convention.TestBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.client.MockRestServiceServer;
 import org.springframework.test.web.client.response.MockRestResponseCreators;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import java.util.Random;
 
@@ -25,39 +26,42 @@ import static org.springframework.test.web.client.match.MockRestRequestMatchers.
 import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
 
 @SpringBootTest(classes = DemoApplication.class)
-@WebMvcTest
-@ExtendWith(SpringExtension.class)
+@EnableWebMvc
 public class BasicFunctionalityTests {
   @Autowired
   private MockMvc mockMvc;
 
-  @MockBean
-  private final ApiRequestServiceProperties properties = new ApiRequestServiceProperties(
+  @TestBean
+  private ApiRequestServiceProperties properties;
+  static ApiRequestServiceProperties properties() {
+    return new ApiRequestServiceProperties(
         "api-endpoint",
         "token-endpoint",
         "API_USERNAME",
         "API_PASSWORD",
         3);
+  }
   RestTemplate restTemplate = new RestTemplate();
   MockRestServiceServer mockServer = MockRestServiceServer.bindTo(restTemplate).build();
   ObjectMapper objMapper = new ObjectMapper();
 
   @Test
-  void testApiToken() {
+  void testApiTokenRequest() {
+
     Random rng = new Random();
     ApiKeyResponse fastExpiryMockKey = new ApiKeyResponse(
         "cooltoken" + rng.nextInt(),
         10, 10,
-        "lesscooltoken" + rng.nextInt(),
+        "anothercooltoken" + rng.nextInt(),
         "Bearer", "1",
         "qwerty", "profile email");
-    try {
-      mockServer
-          .expect(requestTo(properties.tokenEndpointUrl()))
-          .andRespond(withSuccess(objMapper.writeValueAsString(fastExpiryMockKey), MediaType.APPLICATION_JSON));
-    } catch (JsonProcessingException ignored) {
-
-    }
+//    try {
+//      mockServer
+//          .expect(requestTo(properties.tokenEndpointUrl()))
+//          .andRespond(withSuccess(objMapper.writeValueAsString(fastExpiryMockKey), MediaType.APPLICATION_JSON));
+//    } catch (JsonProcessingException ignored) {
+//
+//    }
 
   }
 }
