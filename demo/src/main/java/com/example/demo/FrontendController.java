bbb8a1b58a3b30f13d93d8e07992757d99a6a2cb
Cypher Cola <cypherco@student.21-school.ru>
2025-01-25 13:54:47 +0900

    a

--- demo/src/main/java/com/example/demo/FrontendController.java
+++ demo/src/main/java/com/example/demo/FrontendController.java
@@ -1,14 +1,18 @@
 package com.example.demo;
 
+import com.example.demo.data.PeerDataRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class FrontendController {
+  @Autowired
+  private PeerDataRepository peerRepo;
   @GetMapping("/")
   String index(Model model) {
-    model.addAttribute("asd", "asd");
+    model.addAttribute("users", peerRepo.findAll());
     return "index";
   }
 }
