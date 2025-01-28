56de86f56fc1cb0c3a046d09cab7c4e20ffabb8a
Cypher Cola <cypherco@student.21-school.ru>
2025-01-28 16:04:59 +0900

    123

--- demo/src/main/java/com/example/demo/FrontendController.java
+++ demo/src/main/java/com/example/demo/FrontendController.java
@@ -1,11 +1,14 @@
 package com.example.demo;
 
+import com.example.demo.data.Peer;
 import com.example.demo.data.PeerBaseDataRepository;
 import com.example.demo.data.PeerMutableDataRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.stream.StreamSupport;
 
 @Controller
 public class FrontendController {
@@ -15,7 +18,7 @@ public class FrontendController {
   private PeerMutableDataRepository mutableRepo;
   @GetMapping("/")
   String index(Model model) {
-    model.addAttribute("users", baseRepo.findAll());
+    model.addAttribute("users", StreamSupport.stream(baseRepo.findAll().spliterator(), false).map((p) -> Peer.combine(p, mutableRepo.findById(p.id()).get())));
     return "index";
   }
 }
