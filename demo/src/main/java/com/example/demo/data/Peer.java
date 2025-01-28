5fc1f2155e6e2373d066cfa4e7673f9904111b18
Cypher Cola <cypherco@student.21-school.ru>
2025-01-28 16:04:59 +0900

    123

---
+++ demo/src/main/java/com/example/demo/data/Peer.java
@@ -0,0 +1,18 @@
+package com.example.demo.data;
+
+import org.springframework.data.relational.core.mapping.Column;
+
+public record Peer(String login,
+                   String wave,
+                   int intensive,
+                   int tribeId,
+                   PeerState state,
+                   int tribePoints,
+                   int expValue,
+                   int peerReviewPoints,
+                   int codeReviewPoints,
+                   int coins) {
+  public static Peer combine(PeerBaseData bd, PeerMutableData md) {
+    return new Peer(bd.login(), bd.wave(), bd.intensive(), bd.tribeId(), md.state(), md.tribePoints(), md.expValue(), md.peerReviewPoints(), md.codeReviewPoints(), md.coins());
+  }
+}
