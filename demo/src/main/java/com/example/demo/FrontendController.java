e0375bd471a9cf74e6c63547e6bc5327bb9754d0
Cypher Cola <cypherco@student.21-school.ru>
2025-01-31 17:34:05 +0900

    hmm

--- demo/src/main/java/com/example/demo/FrontendController.java
+++ demo/src/main/java/com/example/demo/FrontendController.java
@@ -1,13 +1,19 @@
 package com.example.demo;
 
 import com.example.demo.data.Peer;
+import com.example.demo.data.PeerBaseData;
 import com.example.demo.data.PeerBaseDataRepository;
 import com.example.demo.data.PeerMutableDataRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.List;
 import java.util.stream.StreamSupport;
 
 @Controller
@@ -17,8 +23,13 @@ public class FrontendController {
   @Autowired
   private PeerMutableDataRepository mutableRepo;
   @GetMapping("/")
-  String index(Model model) {
-    model.addAttribute("users", StreamSupport.stream(baseRepo.findAll().spliterator(), false).map((p) -> Peer.combine(p, mutableRepo.findById(p.id()).get())));
+  String index(@RequestParam(defaultValue = "1") int page, Model model) {
+    int totalPages = baseRepo.findAll(Pageable.ofSize(50)).getTotalPages();
+    model.addAttribute("currentPage", 1);
+    model.addAttribute("totalPages", totalPages);
+    model.addAttribute("uniqueWaves", baseRepo.findDistinctWaves());
+    model.addAttribute("uniqueTribes", baseRepo.findDistinctTribes());
+    model.addAttribute("peers", StreamSupport.stream(baseRepo.findAll().spliterator(), false).toList());
     return "index";
   }
 }
--- demo/src/main/java/com/example/demo/data/PeerBaseDataRepository.java
+++ demo/src/main/java/com/example/demo/data/PeerBaseDataRepository.java
@@ -1,5 +1,7 @@
 package com.example.demo.data;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
@@ -9,4 +11,11 @@ import java.util.List;
 public interface PeerBaseDataRepository extends CrudRepository<PeerBaseData, Integer>, PagingAndSortingRepository<PeerBaseData, Integer> {
   @Query("SELECT login FROM peer_base_data")
   List<String> getAllLogins();
+  @Query("SELECT DISTINCT wave FROM peer_base_data")
+  List<String> findDistinctWaves();
+  @Query("SELECT DISTINCT tribe_data.id, tribe_data.name FROM peer_base_data JOIN tribe_data ON peer_base_data.tribe_id = tribe_data.id")
+  List<TribeData> findDistinctTribes();
+  List<PeerBaseData> findByWave(String wave, Pageable page);
+  List<PeerBaseData> findByTribeId(int tribeId, Pageable page);
+  List<PeerBaseData> findByWaveAndTribeId(String wave, int tribeId, Pageable page);
 }
