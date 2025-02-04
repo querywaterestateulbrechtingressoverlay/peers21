2d474db4bf53f642da6182a36977ad0db523e3fb
Cypher Cola <cypherco@student.21-school.ru>
2025-02-04 18:52:21 +0900

    omegalul2

---
+++ demo/src/main/java/com/example/demo/security/AppSecurityConfig.java
@@ -0,0 +1,24 @@
+package com.example.demo.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class AppSecurityConfig {
+//  @Bean
+//  public SecurityFilterChain securityConfig(HttpSecurity http) throws Exception {
+//    http.addFilter()
+//  }
+//  @Bean
+//  public UserDetailsManager userDetailsService() {
+//    return new InMemoryUserDetailsManager(User.builder().username("user").build());
+//  }
+}
