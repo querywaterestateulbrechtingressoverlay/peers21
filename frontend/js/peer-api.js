ca15df5734b980d0b31a7bc2d91ae1ef46113f3d
Cypher Cola <cypherco@student.21-school.ru>
2025-02-08 19:26:06 +0900

    frontend started

---
+++ frontend/js/peer-api.js
@@ -0,0 +1,41 @@
+async function getPeerData() {
+  const apiUrl = "http://127.0.0.1:8080/api/peers";
+  const apiUsername = "abbottme";
+  const apiPassword = "password";
+  const headers = new Headers();
+  headers.set(
+    "Authorization", "Basic " + btoa(apiUsername + ":" + apiPassword)
+  );
+  try {
+    console.log("*" + btoa(apiUsername + ":" + apiPassword) + "*");
+    const peerResponse = await fetch(apiUrl, {
+      headers: headers
+    });
+    // if (!peerResponse.ok) {
+    //   console
+    //   console.log(peerResponse.headers);
+    //   console.log(peerResponse.body);
+    //   throw new Error(`Response status: ${peerResponse.status}`);
+    // }
+    const json = await peerResponse.json();
+    console.log(json);
+
+    const tableBody = document.querySelector("#peer-table tbody");
+    tableBody.innerHTML = "";
+    json.forEach(peer => {
+      const row = document.createElement("tr");
+      row.innerHTML = `
+        <tr>${peer.login}</tr>
+        <tr>${peer.wave}</tr>
+        <tr>${peer.tribeId}</tr>
+        <tr>${peer.expValue}</tr>
+        <tr>${peer.peerReviewPoints}</tr>
+        <tr>${peer.codeReviewPoints}</tr>
+      `
+      tableBody.appendChild(row);
+    })
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+window.onload = getPeerData;
