e3e97c28260bdce550d6a3800c132cbf87e1ffb2
Cypher Cola <cypherco@student.21-school.ru>
2025-02-15 20:33:25 +0900

    fixed tests

--- backend/data-layer/src/main/java/ru/cyphercola/peers21/datalayer/security/AppSecurityConfig.java
+++ backend/data-layer/src/main/java/ru/cyphercola/peers21/datalayer/security/AppSecurityConfig.java
@@ -1,28 +1,41 @@
 package ru.cyphercola.peers21.datalayer.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
 public class AppSecurityConfig {
+  @Autowired
+  Environment environment;
   @Bean
   SecurityFilterChain securityConfig(HttpSecurity http) throws Exception {
-    return http
+    HttpSecurity h = http
       .cors(Customizer.withDefaults())
       .authorizeHttpRequests((auth) -> auth
+        .requestMatchers(HttpMethod.GET, "/ error").permitAll()
+        .requestMatchers(HttpMethod.GET, "/api/error").permitAll()
         .requestMatchers(HttpMethod.GET, "/api/**").hasRole("USER")
         .requestMatchers(HttpMethod.PUT,"/api/**").hasRole("ADMIN")
         .requestMatchers(HttpMethod.DELETE,"/api/**").hasRole("ADMIN")
         .anyRequest().permitAll())
-      .httpBasic(Customizer.withDefaults())
-      .build();
+      .httpBasic(Customizer.withDefaults());
+    if (List.of(environment.getActiveProfiles()).contains("test")) {
+      h.csrf(AbstractHttpConfigurer::disable);
+    }
+    return h.build();
   }
   @Bean
   public UserDetailsService userDetailsService() {
--- backend/data-layer/src/test/java/ru/cyphercola/peers21/datalayer/ApiBehaviourTests.java
+++ backend/data-layer/src/test/java/ru/cyphercola/peers21/datalayer/ApiBehaviourTests.java
@@ -1,10 +1,14 @@
 package ru.cyphercola.peers21.datalayer;
 
+import org.apache.coyote.Response;
+import org.junit.jupiter.api.*;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.*;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import ru.cyphercola.peers21.datalayer.DataLayerApplication;
 import ru.cyphercola.peers21.datalayer.ApiController;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
@@ -15,39 +19,68 @@ import org.springframework.test.context.bean.override.convention.TestBean;
 import org.springframework.test.context.TestPropertySource;
 
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import ru.cyphercola.peers21.datalayer.data.*;
+import ru.cyphercola.peers21.datalayer.dto.TribeDataDTO;
 
 import javax.sql.DataSource;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest(webEnvironment=SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
-@Sql(scripts = {"test_data.sql", "test_schema.sql"})
 @TestPropertySource(locations="classpath:test.properties")
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class ApiBehaviourTests {
   @LocalServerPort
   private int port;
   @Autowired
   private TestRestTemplate template;
-//  @TestBean
-//  DataSource testDataSource;
-//  static DataSource testDataSource() {
-//    return new EmbeddedDatabaseBuilder()
-//        .generateUniqueName(true)
-//        .setType(EmbeddedDatabaseType.H2)
-////        .addDefaultScripts()
-//        .addScript("test_schema.sql")
-//        .addScript("test_data.sql")
-//        .build();
-//  }
-
+  @Autowired
+  private ApiUserRepository apiUserRepository;
   @Test
-  void testPing() throws Exception {
-//    try (java.sql.Connection c = testDataSource.getConnection("sa","")) {
-//      System.out.println(c.createStatement().executeQuery("SELECT * FROM api_users LIMIT 1;").getString(1));
-//    }
-    String maybePong = this.template.withBasicAuth("user", "userpassword").getForObject("http://localhost:" + port + "/api/ping", String.class);
-    System.out.println(maybePong);
+  @Order(1)
+  void pingUnauthorized() {
+    ResponseEntity<String> pongEntity = this.template
+      .getForEntity("http://localhost:" + port + "/api/ping", String.class);
+    assertThat(pongEntity.getStatusCode()).isEqualTo(HttpStatus.UNAUTHORIZED);
+  }
+  @Test
+  @Order(2)
+  void pingAuthorized() {
+    String maybePong = this.template
+      .withBasicAuth("user", "userpassword")
+      .getForObject("http://localhost:" + port + "/api/ping", String.class);
     assertThat(maybePong).isEqualTo("pong");
+  }
+  @Test
+  @Order(3)
+  void getTribes() {
+    ResponseEntity<List<TribeData>> getTribeDataRE = this.template
+      .withBasicAuth("user", "userpassword")
+      .exchange("http://localhost:" + port + "/api/tribes", HttpMethod.GET, null, new ParameterizedTypeReference<List<TribeData>>() {});
+    assertThat(getTribeDataRE.getStatusCode()).isEqualTo(HttpStatus.OK);
+    assertThat(getTribeDataRE.getBody().size()).isEqualTo(2);
+    assertThat(getTribeDataRE.getBody().stream().filter((td) -> td.tribeId() == 123 && td.name().equals("amogi")).count()).isEqualTo(1);
+    assertThat(getTribeDataRE.getBody().stream().filter((td) -> td.tribeId() == 321 && td.name().equals("abobi")).count()).isEqualTo(1);
+  }
+  @Test
+  @Order(4)
+  void putTribesUnauthorizedUser() {
+    HttpEntity<List<TribeDataDTO>> tribeDataToPut = new HttpEntity<>(List.of(new TribeDataDTO(222, "asd")));
+    ResponseEntity<Void> putTribeDataRE = this.template
+      .withBasicAuth("user", "userpassword")
+      .exchange("http://localhost:" + port + "/api/tribes", HttpMethod.PUT, tribeDataToPut, Void.class);
+    assertThat(putTribeDataRE.getStatusCode()).isEqualTo(HttpStatus.FORBIDDEN);
+  }
+  @Test
+  @Order(5)
+  void putTribesAuthorizedUser() {
+    HttpEntity<List<TribeDataDTO>> tribeDataToPut = new HttpEntity<>(List.of(new TribeDataDTO(222, "asd")));
+    ResponseEntity<Void> putTribeDataRE = this.template
+      .withBasicAuth("admin", "adminpassword")
+      .exchange("http://localhost:" + port + "/api/tribes", HttpMethod.PUT, tribeDataToPut, Void.class);
+    assertThat(putTribeDataRE.getStatusCode()).isEqualTo(HttpStatus.OK);
   }
 }
